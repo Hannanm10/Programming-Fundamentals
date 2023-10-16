9f13358255c8c3a401333b1ace86d5b49678df1b
Hannanm10 <144413909+Hannanm10@users.noreply.github.com>
2023-10-16 22:37:38 +0500

    Update task13.cpp

--- PF WEEK 6/task13.cpp
+++ PF WEEK 6/task13.cpp
@@ -1,31 +1,32 @@
 #include<iostream>
 using namespace std;
 
-float lowestPrice(int, string);
+float lowestPrice(int km,string period);
 
 main(){
+    
     string period;
     int km;
-    cout<<"Enter the number of kilometers: ";
-    cin>> km;
-    cout<<"Enter the period of the day (day/night): ";
-    cin>> period;
-    float price;
-    price = lowestPrice(km , period);
-    cout<<"Lowest price for "<<km<<" kilometers: "<<price<<" EUR";
+    cout << "Enter the number of kilometers: ";
+    cin >> km;
+    cout << "Enter the period of the day (day/night): ";
+    cin >> period;
+    float price = lowestPrice(km,period);
+    cout << "Lowest price for "<< km <<" kilometers: " << price<<" EUR";
 }
 
-float lowestPrice(int km , string period){
+float lowestPrice(int km ,string period){
+    
     float lowest;
     float taxiday = 0.79;
     float taxinight = 0.90;
     float busprice = 0.09;
     float trainprice = 0.06;
     if (period == "day" && (km > 0 && km < 20)){
-        lowest = 0.70 + taxiday*km;
+        lowest = 0.70+taxiday*km;
     }
     if (period == "night" && (km > 0 && km < 20)){
-        lowest = 0.70 + taxinight*km;
+        lowest = 0.70+taxinight*km;
     }
     if ((period == "day" || period == "night") && km >= 20 ){
         lowest = busprice*km;
