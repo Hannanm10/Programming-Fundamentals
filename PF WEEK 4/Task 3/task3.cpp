803e44b395da261949ee54913d5762fd9f27e4d6
Hannanm10 <144413909+Hannanm10@users.noreply.github.com>
2023-10-21 15:41:36 +0500

    Update task3.cpp

--- PF WEEK 4/Task 3/task3.cpp
+++ PF WEEK 4/Task 3/task3.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 using namespace std;
 
-void stickers();
+void stickers(float side);
 
 main () {
 	
 	cout << "Enter the side length of the Rubik's Cube: ";
 	float side;
 	cin >> side;
-	stickers();
+	stickers(side);
 }
 
-void stickers() {
+void stickers(float side) {
 
 	float stickers = side*(6*side);
 	cout << "Number of stickers needed: " <<stickers;
